89a53a1fb74edd8311871a53825a642d8bd90bba
Matthew Garelli <mrgarelli@gmail.com>
2022-07-29 12:53:57 -0500

    updated shell initialization to not use setopt add t function add beginning of convenience functions

--- where-to-cli/cmd/init.go
+++ where-to-cli/cmd/init.go
@@ -16,17 +16,44 @@ var initCmd = &cobra.Command{
 	Short: "initializes the 'to' command in the shell with completions",
 	Long:  `initializes the 'to' command in the shell with completions`,
 	Run: func(cmd *cobra.Command, args []string) {
-		zsh_init := `function go_to() {
-  headed=$(find-where-to-go $1)
+		zsh_init := `
+function t() {
+  to_go_to="${1}";
+  find-where-to-go isdir $to_go_to
+  if [ $? -eq 0 ]; then
+    clear
+    if command -v exa &> /dev/null; then 
+      exa --tree --level=1 --group-directories-first ${to_go_to}
+    elif command -v tree &> /dev/null; then 
+      tree -C -L 1 --dirsfirst ${to_go_to}
+    else
+      ls --color --group-directories-first -1 ${to_go_to}
+    fi
+  fi
+}
+
+function dn() {
+  to_go_to="${1}";
+  find-where-to-go isdir $to_go_to
+
+  if [ $? -eq 0 ]; then
+    if [ $# -eq 0 ]; then 
+      to_go_to=${HOME};
+    fi;
+    builtin cd "${to_go_to}"; t
+  fi
+}
+alias up="cd ..; t"
+
+function to() {
+  headed=$(find-where-to-go convert $1)
   if ! [ -z $headed ]; then
     builtin cd "${headed}"
     clear; exa --tree --level=1 --group-directories-first
   fi
 }
-alias to=go_to
 
-
-setopt completealiases`
+`
 		fmt.Println(zsh_init)
 	},
 }
