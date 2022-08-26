cba1b26bd278e7b24de7257104ea4cc3ccce2935
nanvenomous <mrgarelli@gmail.com>
2022-08-25 19:54:39 -0500

    finished add command

--- where-to-cli/cmd/add.go
+++ where-to-cli/cmd/add.go
@@ -5,6 +5,7 @@ package cmd
 
 import (
 	"errors"
+	"path/filepath"
 
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
@@ -16,10 +17,18 @@ var addCmd = &cobra.Command{
 	Short: "<alias> <path> add alias to path so you can quickly jump there",
 	Long:  `<alias> <path> add alias to path so you can quickly jump there`,
 	RunE: func(cmd *cobra.Command, args []string) error {
+		var (
+			err    error
+			absPth string
+		)
 		if len(args) != 2 {
 			return errors.New("expected two arguments <alias> <path>")
-
 		}
+		absPth, err = filepath.Abs(args[1])
+		if err != nil {
+			return err
+		}
+		viper.Set(args[0], absPth)
 		viper.WriteConfig()
 		return nil
 	},
